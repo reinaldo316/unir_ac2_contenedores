cf58a3dd946d6448686aada96e970b2178824841
Edgar guerra <reinaldo316@gmail.com>
2023-05-14 14:40:40 -0500

    correciones varias

--- index.js
+++ index.js
@@ -8,18 +8,35 @@ const Animal = mongoose.model('Animal', new mongoose.Schema({
 
 const app = express();
 
-mongoose.connect('mongodb://root:password@mongo:27017/miapp?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://root:password@mongo:27017/miapp?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error: ', err));
 
 app.get('/', async (_req, res) => {
   console.log('listando... chanchitos...');
-  const animales = await Animal.find();
-  return res.send(animales);
+  try {
+    const animales = await Animal.find();
+    return res.send(animales);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('Error al obtener animales');
+  }
 });
 
 app.get('/crear', async (_req, res) => {
   console.log('creando...');
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' });
-  return res.send('ok');
+  try {
+    await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' });
+    return res.send('ok');
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('Error al crear animal');
+  }
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection:', err);
+  process.exit(1);
 });
 
 app.listen(3000, () => console.log('listening...'));
